ccfaf8299f75e073feba9963fa672082300a38eb
Anatol Sialitski <asi@enonic.com>
2021-03-23 11:29:39 +0300

    Upgrade to new Guillotine 5.0 #5

--- src/main/resources/controllers/graphql.js
+++ src/main/resources/controllers/graphql.js
@@ -1,9 +1,7 @@
 var guillotineLib = require('/lib/guillotine');
 var graphqlPlaygroundLib = require('/lib/graphql-playground');
 var graphQlLib = require('/lib/graphql');
-var graphQlRxLib = require('/lib/graphql-rx');
 var authLib = require('/lib/xp/auth');
-var webSocketLib = require('/lib/xp/websocket');
 
 
 //──────────────────────────────────────────────────────────────────────────────
@@ -28,53 +26,6 @@ function isAuthenticated() {
 function canAccessAdminLogin() {
     return authLib.hasRole('system.admin') || authLib.hasRole('system.admin.login')
 }
-
-
-
-//──────────────────────────────────────────────────────────────────────────────
-// GraphQL WS Protocol
-//──────────────────────────────────────────────────────────────────────────────
-const graphQlSubscribers = {};
-
-function cancelSubscription(sessionId) {
-    Java.synchronized(() => {
-        const subscriber = graphQlSubscribers[sessionId];
-        if (subscriber) {
-            delete  graphQlSubscribers[sessionId];
-            subscriber.cancelSubscription();
-        }
-    }, graphQlSubscribers)();
-}
-
-function handleStartMessage(sessionId, message) {
-    const graphlqlOperationId = message.id;
-    const payload = message.payload;
-
-    try {
-        const result = graphQlLib.execute(SCHEMA, payload.query, payload.variables);
-
-        if (result.data instanceof com.enonic.lib.graphql.rx.Publisher) {
-            const subscriber = graphQlRxLib.createSubscriber({
-                onNext: (result) => {
-                    webSocketLib.send(sessionId, JSON.stringify({
-                        type: 'data',
-                        id: graphlqlOperationId,
-                        payload: result
-                    }));
-                }
-            });
-            Java.synchronized(() => graphQlSubscribers[sessionId] = subscriber, graphQlSubscribers)();
-            result.data.subscribe(subscriber);
-        }
-    } catch (e) {
-        log.error('Error while handling Start GraphQL-WS message', e);
-        throw e;
-    }
-}
-
-
-
-
 
 //──────────────────────────────────────────────────────────────────────────────
 // Error handling
@@ -106,7 +57,6 @@ function createUnauthorizedError() {
         }
     }
 }
-
 
 
 function createForbiddenError() {
@@ -161,36 +111,9 @@ exports.post = function (req) {
     return {
         contentType: 'application/json',
 		headers: CORS_HEADERS,
-        body: graphQlLib.execute(SCHEMA, body.query, body.variables)
+        body: JSON.stringify(graphQlLib.execute(SCHEMA, body.query, body.variables))
     };
 };
 
 
-exports.webSocketEvent = function (event) {
-    switch (event.type) {
-    case 'close':
-        cancelSubscription(event.session.id);
-        break;
-    case 'message':
-        var message = JSON.parse(event.message);
-        switch (message.type) {
-        case 'connection_init':
-            webSocketLib.send(event.session.id, JSON.stringify({
-                type: 'connection_ack'
-            }));
-            break;
-        case 'start':
-            handleStartMessage(event.session.id, message);
-            break;
-        case 'stop':
-            cancelSubscription(event.session.id);
-            break;
-        }
-        break;
-    case 'error':
-        log.warning('Session [' + event.session.id + '] error: ' + event.error);
-        break;
-    }
-};
-
-
+exports.webSocketEvent = guillotineLib.initWebSockets(SCHEMA);

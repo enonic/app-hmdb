9946e07605f9c099f75d53fb7e46950a667e44c7
Anatol Sialitski <asi@enonic.com>
2021-05-31 15:44:22 +0300

    Bump version from 2.0.2-SNAPSHOT to 2.1.0-SNAPSHOT

--- src/main/resources/controllers/graphql.js
+++ src/main/resources/controllers/graphql.js
@@ -1,105 +1,36 @@
-var guillotineLib = require('/lib/guillotine');
-var graphqlPlaygroundLib = require('/lib/graphql-playground');
-var graphQlLib = require('/lib/graphql');
-var authLib = require('/lib/xp/auth');
-
+const guillotineLib = require('/lib/guillotine');
+const graphqlPlaygroundLib = require('/lib/graphql-playground');
 
 //──────────────────────────────────────────────────────────────────────────────
 // Constants
 //──────────────────────────────────────────────────────────────────────────────
-var CORS_HEADERS = {
-	'Access-Control-Allow-Headers': 'Content-Type',
-	'Access-Control-Allow-Methods': 'POST, OPTIONS',
-	'Access-Control-Allow-Origin': '*'
+const CORS_HEADERS = {
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Methods': 'POST, OPTIONS',
+    'Access-Control-Allow-Origin': '*'
 };
-
-var SCHEMA = guillotineLib.createSchema();
-
-
-//──────────────────────────────────────────────────────────────────────────────
-// Authentication
-//──────────────────────────────────────────────────────────────────────────────
-function isAuthenticated() {
-    return authLib.hasRole('system.authenticated')
-}
-
-function canAccessAdminLogin() {
-    return authLib.hasRole('system.admin') || authLib.hasRole('system.admin.login')
-}
-
-//──────────────────────────────────────────────────────────────────────────────
-// Error handling
-//──────────────────────────────────────────────────────────────────────────────
-function createNotFoundError() {
-    return {
-        status: 404,
-        body: {
-            "errors": [
-                {
-                    "errorType": "404",
-                    "message": "Not found"
-                }
-            ]
-        }
-    }
-}
-
-function createUnauthorizedError() {
-    return {
-        status: 401,
-        body: {
-            "errors": [
-                {
-                    "errorType": "401",
-                    "message": "Unauthorized"
-                }
-            ]
-        }
-    }
-}
-
-
-function createForbiddenError() {
-    return {
-        status: 403,
-        body: {
-            "errors": [
-                {
-                    "errorType": "403",
-                    "message": "Forbidden"
-                }
-            ]
-        }
-    }
-}
-
 
 //──────────────────────────────────────────────────────────────────────────────
 // Methods
 //──────────────────────────────────────────────────────────────────────────────
 exports.options = function () {
-	return {
-		contentType: 'text/plain;charset=utf-8',
-		headers: CORS_HEADERS
-	};
+    return {
+        contentType: 'text/plain;charset=utf-8',
+        headers: CORS_HEADERS
+    };
 };
 
 exports.get = function (req) {
     if (req.webSocket) {
-        if (!isAuthenticated()) {
-            return createUnauthorizedError();
-        }
-        if (!canAccessAdminLogin()) {
-            return createForbiddenError();
-        }
         return {
             webSocket: {
+                data: guillotineLib.createWebSocketData(req),
                 subProtocols: ['graphql-ws']
             }
         };
     }
 
-    var body = graphqlPlaygroundLib.render();
+    let body = graphqlPlaygroundLib.render();
     return {
         contentType: 'text/html; charset=utf-8',
         body: body
@@ -107,13 +38,18 @@ exports.get = function (req) {
 };
 
 exports.post = function (req) {
-    var body = JSON.parse(req.body);
+    let input = JSON.parse(req.body);
+
+    let params = {
+        query: input.query,
+        variables: input.variables
+    };
+
     return {
         contentType: 'application/json',
-		headers: CORS_HEADERS,
-        body: JSON.stringify(graphQlLib.execute(SCHEMA, body.query, body.variables))
+        headers: CORS_HEADERS,
+        body: guillotineLib.execute(params)
     };
 };
 
-
-exports.webSocketEvent = guillotineLib.initWebSockets(SCHEMA);
+exports.webSocketEvent = guillotineLib.initWebSockets();

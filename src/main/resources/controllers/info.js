07bfef5f308bf902c83ef4f0e0f2e32ca4fa9c25
Thomas Sigdestad <tsi@enonic.com>
2022-04-05 14:23:11 +0200

    Moved /api to /_graphql to avoid conflicts (#34)

--- src/main/resources/controllers/info.js
+++ src/main/resources/controllers/info.js
@@ -3,19 +3,20 @@ var portal = require('/lib/xp/portal');
 exports.get = function (req) {
     var title = "Headless Movie Database";
     var heading = "Welcome to the Headless Movie Database";
+    var preview = "Click \"Preview\" button in Content Studio access the API.";
     var assetUrl = portal.assetUrl({
         path: 'styles.css'
       });
     var siteUrl = portal.pageUrl({
         id: portal.getSite()._id
       });
-    var apiUrl = siteUrl+"/api";
+    var apiUrl = siteUrl+"/_graphql";
     var baseUrl = req.scheme+"://"+req.host+":"+req.port;
     var sdkMode = (req.host === 'localhost' || req.host === '127.0.0.1') ? true : false;
     var draftPath = "/site/hmdb/draft/hmdb";
-    var draftApi = baseUrl+draftPath+"/api";
+    var draftApi = baseUrl+draftPath+"/_graphql";
     var masterPath = "/site/hmdb/master/hmdb";
-    var masterApi = baseUrl+masterPath+"/api";
+    var masterApi = baseUrl+masterPath+"/_graphql";
     var branch = req.branch;
     var mode = req.mode;
     var vhostDocs = "https://developer.enonic.com/docs/xp/stable/deployment/vhosts";
@@ -41,8 +42,8 @@ exports.get = function (req) {
         <link rel="stylesheet" type="text/css" href="${assetUrl}"/>
       </head>
       <body>
+          <strong>${preview}</strong>
           <h1>${heading}</h1>
-          <p><strong>NOTE:</strong> You may instantly access the API by clicking the Content Studio "Preview" button above.</p>
           <h3>Enonic Cloud Demo</h3>
           <p>If this application was installed as part of an Enonic Cloud demo, you may access the draft and live APIs on public URLs by visiting one of the solution's pre-defined routes.</p>
           <h3>Other deployments</h3>
@@ -63,9 +64,10 @@ exports.get = function (req) {
         <link rel="stylesheet" type="text/css" href="${assetUrl}"/>
       </head>
       <body>
+          <strong>${preview}</strong>
           <h1>${heading}</h1>
-          You appear to be running this app as a developer.
-          Visit the links below in a new browser tab to access the Headless API:
+        
+          <p>Use the following internal paths to expose the API to the public:</p>
   
           <h3>Drafts API:</h3>
           ${draftApi}
@@ -74,22 +76,26 @@ exports.get = function (req) {
           ${masterApi}
      
           <h3>Pretty URLs</h3>
-          When deploying to a server, use <a target="_blank" href="${vhostDocs}">vhosts</a> to create custom URLs like "example.com/api".
+          When deploying to a server, use <a target="_blank" href="${vhostDocs}">vhosts</a> to create custom URLs like "example.com/_graphql".
       </body>
     </html>
     `;
 
-    if (sdkMode) {
+    // For all previews
+    if (req.mode == 'preview') {
+      return {
+        body: standard
+      }
+    }
+    // running on localhost
+    else if (sdkMode) {
       return  {
         body: sdk
       }
-    } else if (req.mode == 'inline' || req.mode == 'edit') {
-      return  {
-        body: inline
-      }
-    } else {
+    }
+    else {
       return {
-        body: standard
+        body: inline
       }
     }
     
